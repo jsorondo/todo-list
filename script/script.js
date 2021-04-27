09d7277ecbf247a4f8666e4819a7b5e24b269cc6
Jorge Sorondo <17126240+jsorondo@users.noreply.github.com>
2021-04-26 19:44:26 -0500

    my todo list 1.0

---
+++ script/script.js
@@ -0,0 +1,30 @@
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+} 
+
+// delete item from list after 1 second
+document.querySelector("#myUL").addEventListener('click', function(z){
+
+    z.target.innerHTML = z.target.innerHTML.strike();
+
+    setTimeout(function(){z.target.remove()}, 1000);
+
+});
